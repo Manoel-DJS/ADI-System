c6cc2fbdd1a1593294e07ee00e5197dd200af1d2
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-05-06 11:16:06 -0300

    first Commit / criação da Entity User

---
+++ README.md
@@ -0,0 +1,2 @@
+# ADI-System
+CRUD com Java Spring Boot e MySQL para Aprendizagem - Build &amp; Run
---
+++ src/test/java/tech/buildruin/agregadorInv/AgregadorInvApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildruin.agregadorInv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
